0e840ddc4711df7525a5328ed7a3d9176ff39ab9
Diogo <diogocostasantos1@outlook.pt>
2024-08-08 16:33:09 +0100

    Delete ATM Java directory

--- ATM Java/src/ContaEstudante.java
+++
@@ -1,9 +0,0 @@
-public class ContaEstudante extends Conta {
-
-   public ContaEstudante(String pin, float saldo, long iban, Utilizador titular){
-      super(pin, saldo, iban, titular);
-      this.tipoDeConta="Conta estudante";
-   }
-
-    
-}
--- ATM Java/src/Operacoes.java
+++
@@ -1,5 +0,0 @@
-public interface Operacoes {
-    public void levantamentos(float valor);
-    public void depositos(float valor);
-    public void transferencias(float valor,Conta contaDestino);
-}
